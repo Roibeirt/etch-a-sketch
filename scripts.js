df7d565cb21c0cdd1660c3310a946f8c02b98283
Bobby Aston <bobby.j.aston@gmail.com>
2022-08-15 14:58:33 +0900

    added shader function

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 const sketchContainer = document.querySelector("#sketch-container");
 const setSizeButton = document.querySelector("#setSizeButton");
 const colorInput = document.querySelector("#colorPicker");
+const shadeButton = document.querySelector("#shaderButton")
 
 let sketchDivs = new Array();
 let gridSize = 16;
 setGrid(gridSize);
+let shade = 255;
 
 setSizeButton.addEventListener('click', changeGridSize);
+shadeButton.addEventListener('click', shaderPrep);
 
 function setGrid(size){
 
@@ -39,6 +42,7 @@ function changeGridSize(){
 
         clearGrid();
         setGrid(userInput);
+        gridSize = userInput;
 
     }
     else{
@@ -54,6 +58,21 @@ function changeColor(){
     let currentElement = this;
     currentElement.style.backgroundColor=colorInput.value;
 
+}
+
+function shaderPrep(){
+    for (i=0; i<sketchDivs.length; i++){
+    sketchDivs[i].removeEventListener('mouseenter', changeColor);
+    sketchDivs[i].addEventListener('mouseenter', shader);
+    }
+    shade = 255;
+}
+
+function shader(){
+
+    let currentElement = this;
+    currentElement.style.backgroundColor= 'rgb(' + shade + ',' + shade + ',' + shade + ')';
+    shade -= 25;
 }
 
 
