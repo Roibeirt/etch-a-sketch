ba4296031c8cac29a9044f698bba6213588bf10e
Bobby Aston <bobby.j.aston@gmail.com>
2022-08-16 13:18:57 +0900

    added random colour function and tidied code

--- scripts.js
+++ scripts.js
@@ -1,15 +1,33 @@
+//set constants
+
 const sketchContainer = document.querySelector("#sketch-container");
 const setSizeButton = document.querySelector("#setSizeButton");
 const colorInput = document.querySelector("#colorPicker");
+const rainbowButton = document.querySelector('#randomColor');
 const shadeButton = document.querySelector("#shaderButton")
+
+//add event listeners for buttons
+
+setSizeButton.addEventListener('click', changeGridSize);
+shadeButton.addEventListener('click', shaderPrep);
+rainbowButton.addEventListener('click', rainbowPrep);
+colorInput.addEventListener('change', () => {
+
+    for (i=0; i<sketchDivs.length; i++){
+
+        sketchDivs[i].addEventListener('mouseenter', changeColor);
+    }
+
+} );
+
+//create array to hold grid div elements and create initial grid
 
 let sketchDivs = new Array();
 let gridSize = 16;
 setGrid(gridSize);
-let shade = 255;
 
-setSizeButton.addEventListener('click', changeGridSize);
-shadeButton.addEventListener('click', shaderPrep);
+
+//functions for creating and clearing grid, and changing grid size based on user input
 
 function setGrid(size){
 
@@ -53,6 +71,8 @@ function changeGridSize(){
 
 }
 
+//function to change div colour to colour picker value
+
 function changeColor(){
 
     let currentElement = this;
@@ -60,9 +80,14 @@ function changeColor(){
 
 }
 
+//shading functions
+
+let shade = 255; //set rgb colour value to start as white
+
 function shaderPrep(){
     for (i=0; i<sketchDivs.length; i++){
     sketchDivs[i].removeEventListener('mouseenter', changeColor);
+    sketchDivs[i].removeEventListener('mouseenter', rainbow);
     sketchDivs[i].addEventListener('mouseenter', shader);
     }
     shade = 255;
@@ -72,7 +97,25 @@ function shader(){
 
     let currentElement = this;
     currentElement.style.backgroundColor= 'rgb(' + shade + ',' + shade + ',' + shade + ')';
-    shade -= 25;
+    shade -= (shade/gridSize);
+}
+
+//random colour functions
+
+function rainbowPrep(){
+
+    for (i=0; i<sketchDivs.length; i++){
+        sketchDivs[i].removeEventListener('mouseenter', changeColor);
+        sketchDivs[i].removeEventListener('mouseenter', shader);
+        sketchDivs[i].addEventListener('mouseenter', rainbow);
+        }
+}
+
+function rainbow(){
+
+    let currentElement = this;
+    currentElement.style.backgroundColor= 'rgb(' + (Math.random() * 256) + ',' + (Math.random() * 256) + ',' + (Math.random() * 256) + ')';
+
 }
 
 
